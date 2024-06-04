1d90bf6139cfce9bb8b47e55dbc9c3f91c32f4ea
CilliersWebSolutions <119592115+CilliersWebSolutions@users.noreply.github.com>
2024-06-05 01:03:25 +0200

    made changes to the spagetti code to bring order to the template using classes
    THREE.js Journey lesson on organising code ofr bigger projects

--- src/index.js
+++ src/index.js
@@ -1,205 +1,217 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+
+import Experience from "./Experience/Experience.js";
+
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+// import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  init3D();
-});
-
-
-function init3D() {
-  const containers = document.querySelectorAll('[data-3d="c"]');
-
-
-  // Iterate over each container
-  containers.forEach(container => {
-    // Canvas
-    const canvas = document.createElement('canvas');
-    canvas.classList.add('webgl');
-    container.appendChild(canvas);
-
-    // Scene
-    const scene = new THREE.Scene();
-    /**
-    * Base
-    */
-
-    /**
-     * 
-  * Loaders
-    */
-    // Texture loader
-    const textureLoader = new THREE.TextureLoader()
-
-    // Draco loader
-    const dracoLoader = new DRACOLoader()
-    const devpath = ('')
-    dracoLoader.setDecoderPath('https://cdn.jsdelivr.net/gh/CilliersWebSolutions/draco@main/draco/')
-
-    // GLTF loader
-    const gltfLoader = new GLTFLoader()
-    gltfLoader.setDRACOLoader(dracoLoader)
-
-
-    const bakedTextureRobot = textureLoader.load('https://uploads-ssl.webflow.com/661bd3640db9efe63984e8eb/663e0809e234450ecf26d391_bakedTexture.jpg')
-
-    bakedTextureRobot.flipY = false
-    bakedTextureRobot.colorSpace = THREE.SRGBColorSpace
-    // const bakedTextureMini = textureLoader.load('https://uploads-ssl.webflow.com/661bd3640db9efe63984e8eb/662d5bf6ac9bfbdb074d4d8e_baked.jpg')
-
-    // bakedTextureMini.flipY = false
-    // bakedTextureMini.colorSpace = THREE.SRGBColorSpace
-
-    // const bakedTextureBuilding = textureLoader.load('https://uploads-ssl.webflow.com/661bd3640db9efe63984e8eb/663395f3eb775143e82a5b7c_Building-bakedNew.jpg')
-
-    // bakedTextureBuilding.flipY = false
-    // bakedTextureBuilding.colorSpace = THREE.SRGBColorSpace
-
-    /**
-     * Materials
-     */
-    //Baked material
-    const bakedMaterialRobot = new THREE.MeshBasicMaterial({ map: bakedTextureRobot })
-    //const bakedMaterialMini = new THREE.MeshBasicMaterial({ map: bakedTextureMini })
-    //const bakedMaterialbuilding = new THREE.MeshBasicMaterial({ map: bakedTextureBuilding })
-    //const bakedMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-
-    // Pole Light material
-    const neonLightMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFFFF })
-    //console.log(bakedMaterial)
-    /**
-     *  Model
-     */
-
-    gltfLoader.load(
-      'https://uploads-ssl.webflow.com/661bd3640db9efe63984e8eb/663e1dba2a6df1674f1ff9ab_placeholderRobotCentered.glb.txt',
-      (gltf) => {
-        console.log(gltf)
-
-        const bakedMeshRobot = gltf.scene.children.find(child => child.name === 'robotBaked')
-
-        // const bakedMeshMini = gltf.scene.children.find(child => child.name === 'vendingMachineMini_baked')
-        // const bakedMeshBuilding = gltf.scene.children.find(child => child.name === 'vendingMachineBuilding_baked')
-        // const bakedMeshR1 = gltf.scene.children.find(child => child.name === 'vendingMachine_firstRow vendingMachine_Row8')
-        // const bakedMeshR2 = gltf.scene.children.find(child => child.name === 'vendingMachine_secondRow')
-        // const bakedMeshR3 = gltf.scene.children.find(child => child.name === 'vendingMachine_thirdRow')
-        // const bakedMeshR4 = gltf.scene.children.find(child => child.name === 'vendingMachine_fourthRow')
-        // const bakedMeshR5 = gltf.scene.children.find(child => child.name === 'vendingMachine_topRow')
-        //const neonLightAMesh = gltf.scene.children.find(child => child.name === 'NeonLights_baked')
-        //const poleLightBMesh = gltf.scene.children.find(child => child.name === 'poleLightB')
-        //const portalLightMesh = gltf.scene.children.find(child => child.name === 'portalLight')
-
-        bakedMeshRobot.material = bakedMaterialRobot
-
-        //bakedMeshMini.material = bakedMaterialMini
-        //bakedMeshBuilding.material = bakedMaterialbuilding
-        //neonLightAMesh.material = neonLightMaterial
-
-
-        //bakedMeshR1.material = bakedMaterial
-        //bakedMeshR2.material = bakedMaterial
-        //bakedMeshR3.material = bakedMaterial
-        //bakedMeshR4.material = bakedMaterial
-        //bakedMeshR5.material = bakedMaterial
-        //poleLightBMesh.material = poleLightMaterial
-        //portalLightMesh.material = portalLightMaterial
+  //   init3D();
 
 
 
-
-        scene.add(gltf.scene)
-        console.log(gltf.scene)
-      }
-    )
-
-
-    /**
-     * Sizes
-     */
-    const sizes = {
-      width: container.clientWidth,
-      height: container.clientHeight
-    }
-
-    document.addEventListener('resize', () => {
-      // Update sizes
-      sizes.width = container.clientWidth
-      sizes.height = container.clientHeight
-
-      // Update camera
-      camera.aspect = sizes.width / sizes.height
-      //camera.updateProjectionMatrix()
-
-      // Update renderer
-      renderer.setSize(sizes.width, sizes.height)
-      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-      // // Update fireflies Pixel Ration
-      // firefliesMaterial.uniforms.uPixelRatio.value = Math.min(window.devicePixelRatio, 2)
-    })
-
-    /**
-     * Camera
-     */
-    // Base camera
-    //const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
-    const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
-    camera.position.x = 10
-
-    camera.position.y = 5
-
-    camera.position.z = 5
-
-    scene.add(camera)
+  const baseContainer = document.querySelector('[data-3d="c"]');
+  if (baseContainer) {
+    const experience = new Experience(baseContainer)
+  }
 
 
 
-    // Controls
-    //  const controls = new FirstPersonControls(camera, canvas)
-    const controls = new OrbitControls(camera, canvas)
-    controls.enableDamping = true
-    controls.dampingFactor = 0.009
-    controls.enablePan = false
-    controls.enableZoom = false
+  //   const partContainers = new Experience(document.querySelectorAll('[data-3d="cr"], [data-3d="sp"], [data-3d="gn"], [data-3d="ma"]'))
 
-    /**
-    * Renderer
-    */
-    const renderer = new THREE.WebGLRenderer({
-      canvas: canvas,
-      antialias: true,
-      alpha: true
-    })
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  //   const canvasModels = partContainers.createElement('canvas');
+  //   canvasModels.classList.add('webgl');
+  //   container.appendChild(canvas);
+
+  //   console.log(partContainers)
+
+})
 
 
-    //renderer.setClearColor('#00FFFFFF')
+// function init3D() {
+
+//   const baseContainers = document.querySelectorAll('[data-3d="c"]');
+//   const partContainers = document.querySelectorAll('[data-3d="cr"], [data-3d="sp"], [data-3d="gn"], [data-3d="ma"]');
+
+//   // Create a scene
+//   const scene = new THREE.Scene();
+
+//   /**
+//   * Base
+//   */
+
+//   /**
+//    *
+// * Loaders
+//   */
+//   // Texture loader
+//   const textureLoader = new THREE.TextureLoader()
+
+//   // Draco loader
+//   const dracoLoader = new DRACOLoader()
+//   const devpath = ('')
+//   dracoLoader.setDecoderPath('https://cdn.jsdelivr.net/gh/CilliersWebSolutions/draco@main/draco/')
+
+//   // GLTF loader
+//   const gltfLoader = new GLTFLoader()
+//   gltfLoader.setDRACOLoader(dracoLoader)
+
+//   /**
+//    * Materials
+//    */
+
+//   //Baked material
+
+//   const loadTexture = (url) => {
+//     const texture = textureLoader.load(url);
+//     texture.flipY = false;
+//     texture.colorSpace = THREE.SRGBColorSpace;
+//     return texture;
+//   };
+
+//   const bakedTextureRobotBase = loadTexture('https://uploads-ssl.webflow.com/661bd3640db9efe63984e8eb/664cb285b965e7a8c8c02b55_baseBakedV3.jpg')
+
+//   const bakedTextureRobotAddons = loadTexture('https://uploads-ssl.webflow.com/661bd3640db9efe63984e8eb/664ca1787a5cf33846a2a929_addonBakedFIx.jpg')
+
+//   const bakedMaterialRobotBase = new THREE.MeshBasicMaterial({ map: bakedTextureRobotBase })
+//   const bakedMaterialRobotAddons = new THREE.MeshBasicMaterial({ map: bakedTextureRobotAddons })
+
+//   /**
+//    *  Model
+//    */
+
+//   let baseMesh, addonsMeshes = {};
 
 
-    /**
-    * Animate
-    */
-    const clock = new THREE.Clock()
+//   gltfLoader.load('https://uploads-ssl.webflow.com/661bd3640db9efe63984e8eb/664cd59c394d2c8d2483d381_robotWithAddonsCentered.glb.txt',
+//     (gltf) => {
 
-    const tick = () => {
-      const elapsedTime = clock.getElapsedTime()
+//       baseMesh = gltf.scene.children.find(child => child.name === 'baseBaked');
+
+//       baseMesh.material = bakedMaterialRobotBase;
+
+//       addonsMeshes['cr'] = gltf.scene.children.find(child => child.name === 'sataliteBaked');
+//       addonsMeshes['sp'] = gltf.scene.children.find(child => child.name === 'solarBaked');
+//       addonsMeshes['gn'] = gltf.scene.children.find(child => child.name === 'gunBaked');
+//       addonsMeshes['ma'] = gltf.scene.children.find(child => child.name === 'armBaked');
+
+//       for (const key in addonsMeshes) {
+//         addonsMeshes[key].material = bakedMaterialRobotAddons;
+//       }
+
+//       baseContainers.forEach(container => {
+//         initScene(container, [baseMesh.clone()]);
+//       });
+
+//       partContainers.forEach(container => {
+//         const part = container.getAttribute('data-3d');
+//         if (addonsMeshes[part]) {
+//           initScene(container, [baseMesh.clone(), addonsMeshes[part]]);
+//         }
+//       });
+//     }
+//   );
+// }
+// function initScene(container, meshes) {
+//   const canvas = document.createElement('canvas');
+//   canvas.classList.add('webgl');
+//   container.appendChild(canvas);
+
+//   const scene = new THREE.Scene();
+
+//   meshes.forEach(mesh => {
+//     scene.add(mesh);
+//   });
+//   /**
+//    * Sizes
+//    */
+//   const sizes = {
+//     width: container.clientWidth,
+//     height: container.clientHeight
+//   }
+
+//   document.addEventListener('resize', () => {
+//     // Update sizes
+//     sizes.width = container.clientWidth
+//     sizes.height = container.clientHeight
+
+//     // Materials
 
 
-      // Update controls
-      controls.update()
+//     // Update camera
+//     camera.aspect = sizes.width / sizes.height
 
-      // Render
-      renderer.render(scene, camera)
+//     // Update renderer
+//     renderer.setSize(sizes.width, sizes.height)
+//     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+//   })
 
-      // Call tick again on the next frame
-      window.requestAnimationFrame(tick)
-    }
+//   /**
+//    * Camera
+//    */
 
-    tick()
+//   // Base camera
 
-  });
+//   const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+//   camera.position.x = 10
+
+//   camera.position.y = 5
+
+//   camera.position.z = 5
+
+//   scene.add(camera)
+
+//   // Controls
+
+//   const controls = new OrbitControls(camera, canvas)
+//   controls.enableDamping = true
+//   controls.dampingFactor = 0.009
+//   controls.enablePan = false
+//   controls.enableZoom = false
+
+//   // Check for the specific attribute and enable autoRotate if present
+//   if (container.hasAttribute('data-auto-rotate')) {
+//     controls.autoRotate = true;
+//     controls.autoRotateSpeed = 0.5; // You can adjust the speed
+//   }
+//   /**
+//   * Renderer
+//   */
+//   const renderer = new THREE.WebGLRenderer({
+//     canvas: canvas,
+//     antialias: true,
+//     alpha: true
+//   })
+//   renderer.setSize(sizes.width, sizes.height)
+//   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 
-}
+//   //renderer.setClearColor('#00FFFFFF')
+
+
+//   /**
+//   * Animate
+//   */
+//   const clock = new THREE.Clock()
+
+//   const tick = () => {
+//     const elapsedTime = clock.getElapsedTime()
+
+
+//     // Update controls
+//     controls.update()
+
+//     // Render
+//     renderer.render(scene, camera)
+
+//     // Call tick again on the next frame
+//     window.requestAnimationFrame(tick)
+//   }
+
+//   tick()
+
+//   // });
+
+
+// }
